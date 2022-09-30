2c252cc83c89f00e9cd43196f26e16913807434d
SirWobb79 <111445666+SirWobb79@users.noreply.github.com>
2022-09-30 18:19:05 +0100

    Update Alphaton.py
    0.4 - Random & Math Update

--- Alphaton.py
+++ Alphaton.py
@@ -1,7 +1,8 @@
 from string import ascii_lowercase
 from string import ascii_uppercase
+from random import randint
 
-print("Welcome to Alphaton, the programming language that only uses letters. It is case-sensitive. Type '?' for a list of commands.\n\nLatest Version: 0.3 on 19 Sep 2022")
+print("Welcome to Alphaton, the programming language that only uses letters. It is case-sensitive. Type '?' for a list of commands.\n\nLatest Version: 0.4 on 30 Sep 2022")
 
 chars = ascii_lowercase + ascii_uppercase + " .!?+-×÷\n\t"
 
@@ -20,6 +21,7 @@ i12 = 0
 
 saved = 0
 carry = 0
+temp = 0
 
 ints = [i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12]
 id = 0
@@ -27,14 +29,14 @@ id = 0
 use = True
 while use:
 	
-	print(f"\n\nData Integer Tape:\n{ints}\n\nCurrent D.I (Left -> Right): {id+1}\nSaved Value: {saved}")
+	print(f"\n\nData Integer Tape:\n{ints}\n\nCurrent Data Integer (Left -> Right): {id+1}\nSaved Value: {saved}")
 	
 	command = input("\nMain Command Line >")
 	
 	for char in command:
 		
 		if char == "?":
-			print("\nA: Adds 1 to current integer.\nS: Subtracts 1 from current integer\nL: Move to the next integer to the left.\nR: Move to the next integer to the right.\nP: Prints corrosponding letter of alphabet. Type '*' for the list.\np: Prints current integer value.\nI: Asks for a number. Number will replace current integer value.\ni: Asks for a number. Number will be the selected value on tape.\nE: Resets current integer to 0.\ns: Saves current number to memory\nl: Loads saved number to current position, replacing the previous.\nZ: Transfers current value to the left.\nX: Transfers current value to the right.\nO: Opens loop if value of integer IS NOT 0.\nC: Closes loop if value of integer IS 0. (Loops are NOT stackable)")
+			print("\nA: Adds 1 to current integer.\nS: Subtracts 1 from current integer\nM: Multiplies current integer by saved value.\nD: Floor divides the current integer by the saved value.\nr: 2 integers needed. Random number will be selected from range.\nL: Move to the next integer to the left.\nR: Move to the next integer to the right.\nP: Prints corrosponding letter of alphabet. Type '*' for the list.\np: Prints current integer value.\nI: Asks for a number. Number will replace current integer value.\ni: Asks for a number. Number will be the selected value on tape.\nE: Resets current integer to 0.\ns: Saves current number to memory\nl: Loads saved number to current position, replacing the previous.\ne: Clear saved value.\nZ: Transfers current value to the left.\nX: Transfers current value to the right.\nO: Opens loop if value of integer IS NOT 0.\nC: Closes loop if value of integer IS 0. (Loops are NOT stackable)")
 			
 		if char == "*":
 			print("Alphabet list with corrosponding integer.\n\nA = 1\nB = 2\nC = 3\nD = 4\nE = 5\nF = 6\nG = 7\nH = 8\nI = 9\nJ = 10\nK = 11\nL = 12\nM = 13\nN = 14\nO = 15\nP = 16\nQ = 17\nR = 18\nS = 19\nT = 20\nU = 21\nV = 22\nW = 23\nX = 24\nY = 25\nZ = 26\nCapital letters are 27 -> 52\nSPACE = 53\n. = 54\n! = 55\n? = 56\n+ = 57\n- = 58\n× = 59\n÷ = 60\nNEW LINE = 61\nTAB = 62")
@@ -44,6 +46,19 @@ while use:
 			
 		if char == "S":
 			ints[id] -= 1
+			
+		if char == "M":
+			ints[id] *= saved
+			
+		if char == "D":
+			ints[id] //= saved
+			
+		if char == "r":
+			min = int(input("\nMinimum Int >"))
+			max = int(input("\nMaximum Int >"))
+			
+			temp = randint(min,max)
+			ints[id] = temp
 			
 		if char == "L":
 			if id > 0:
@@ -77,6 +92,9 @@ while use:
 		if char == "l":
 			ints[id] = saved
 			
+		if char == "e":
+			saved = 0
+			
 		if char == "Z":
 			carry = ints[id]
 			ints[id] = 0
@@ -106,6 +124,19 @@ while use:
 			
 						if char2 == "S":
 							ints[id] -= 1
+								
+						if char2 == "M":
+							ints[id] *= saved
+			
+						if char2 == "D":
+							ints[id] //= saved
+							
+						if char2 == "r":
+							min = int(input("\nMinimum Int >"))
+							max = int(input("\nMaximum Int >"))
+			
+							temp = randint(min,max)
+							ints[id] = temp
 			
 						if char2 == "L":
 							if id > 0:
@@ -138,6 +169,9 @@ while use:
 			
 						if char2 == "l":
 							ints[id] = saved
+							
+						if char2 == "e":
+							saved = 0
 			
 						if char2 == "Z":
 							carry = ints[id]
@@ -152,6 +186,7 @@ while use:
 							if id < 11:
 								id += 1
 							ints[id] = carry
+	
 						
 						if char2 == "C":
 							if ints[id] == 0:
